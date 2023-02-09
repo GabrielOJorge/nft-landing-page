d99d6a38d79391fb58ea7b60997db6f0e5c934cb
GabrielOJorge <goliver331@protonmail.com>
2023-02-09 14:56:21 -0300

    feat: prevent default behavior of form

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const hamburgerBtn = document.querySelector(".hamburger-btn");
+const form = document.querySelector("form");
 
 const displayHamburgerMenu = () => {
   hamburgerBtn.classList.toggle("open");
@@ -7,3 +8,4 @@ const displayHamburgerMenu = () => {
 };
 
 hamburgerBtn.addEventListener("click", displayHamburgerMenu);
+form.addEventListener('submit', (e) => e.preventDefault());
