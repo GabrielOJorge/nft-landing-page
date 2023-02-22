a4a538d0393a76c0baf095e6587a4676914b130b
GabrielOJorge <goliver331@protonmail.com>
2023-02-22 20:18:35 -0300

    feat: add scroll animation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const hamburgerBtn = document.querySelector(".hamburger-btn");
+const hiddenElements = document.querySelectorAll(".hidden");
 const form = document.querySelector("form");
 
 const displayHamburgerMenu = () => {
@@ -9,3 +10,13 @@ const displayHamburgerMenu = () => {
 
 hamburgerBtn.addEventListener("click", displayHamburgerMenu);
 form.addEventListener('submit', (e) => e.preventDefault());
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  })
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
